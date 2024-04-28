c6921f11d2189b2154380fa85d64c967b6c44650
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-04-28 09:32:12 -0400

    Activity comlete

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -105,6 +105,21 @@ function PlaceDetails() {
 		})
 	}
 
+	let placeActions = null
+
+	if (currentUser?.role === 'admin') {
+	placeActions = (
+		<>
+			<a className="btn btn-warning" onClick={editPlace}>
+				Edit
+			</a>
+			<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+				Delete
+			</button>
+		</>
+	)
+	}
+
 	return (
 		<main>
 			<div className="row">
@@ -131,6 +146,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
+					{placeActions}
 					<a className="btn btn-warning" onClick={editPlace}>
 						Edit
 					</a>{` `}
